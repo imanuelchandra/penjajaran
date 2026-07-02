0ec4bd5149fc973311c45fa8522aca3c5e4a079b
unknown <lefta.chandra@gmail.com>
2026-07-02 21:58:59 +0900

    refactoring code dan sesuaikan ke bulian d ace

--- penjajaran.plugin.php
+++ penjajaran.plugin.php
@@ -11,7 +11,7 @@ use SLiMS\Plugins;
 
 $plugin = Plugins::getInstance();
 
-Plugins::getInstance()->registerAutoload(__DIR__);
+//Plugins::getInstance()->registerAutoload(__DIR__);
 
 
 $pathPenjajaran = __DIR__ . '/pages/daftar_penjajaran.php';
